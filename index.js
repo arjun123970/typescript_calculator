f05ff9b0db3181c6846e24dbc65b65367c9a241b
Arjun Majety <arjunmajety07@gmail.com>
2022-02-05 19:43:35 -0500

    Added in the logic for the calculator

--- index.js
+++ index.js
@@ -7,4 +7,20 @@ function main() {
     var opperator = (0, readline_sync_1.question)('What would you like to do? Press a for addition, m for multiplication, s for subtraction, and d for division ');
     var firstnum = parseInt(firststr, 10);
     var secondnum = parseInt(secstr, 10);
+    if (opperator == 'a') {
+        console.log(firstnum + secondnum);
+    }
+    else if (opperator == 'm') {
+        console.log(firstnum * secondnum);
+    }
+    else if (opperator == 's') {
+        console.log(firstnum - secondnum);
+    }
+    else if (opperator == 'd') {
+        console.log(firstnum / secondnum);
+    }
+    else {
+        console.log('That is not a valid opperation, please restart the program and use one of the given opperators');
+    }
 }
+main();
--- index.ts
+++ index.ts
@@ -8,5 +8,30 @@ function main(): void
     const opperator: string = question('What would you like to do? Press a for addition, m for multiplication, s for subtraction, and d for division ');
     const firstnum = parseInt(firststr,10);
     const secondnum = parseInt(secstr,10);
+        if (opperator == 'a') 
+    {
+        console.log(firstnum + secondnum);
+    } 
 
+    else if (opperator == 'm')
+    {
+        console.log(firstnum * secondnum);
+    }
+
+    else if (opperator == 's')
+    {
+        console.log(firstnum - secondnum);
+    }
+
+    else if(opperator == 'd')  
+    {
+        console.log(firstnum/secondnum);
+    }
+
+    else
+    {
+        console.log('That is not a valid opperation, please restart the program and use one of the given opperators');
+    }
 }
+
+main();
