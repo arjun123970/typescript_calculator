5582d429d816433ab6ed5ef7247acbf4e49fbfe4
Arjun Majety <arjunmajety07@gmail.com>
2022-02-12 22:18:07 -0500

    Created tests for cypress framework

---
+++ cypress/integration/untitled.spec.js
@@ -0,0 +1,9 @@
+const { sum, sub, multiplication, division} = require('C:/Users/Chandra/Desktop/typescript_calculator/index.js');
+describe('Calculator functions test', () => {
+    it('This will test out the operation functions on my calculator program', () => {
+        expect(sum(1, 2)).to.equal(3);
+        expect(sub(1, 2)).to.equal(-1);
+        expect(multiplication(1, 2)).to.equal(2);
+        expect(division(2, 1)).to.equal(2);
+    })
+  })
